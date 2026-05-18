6fe9f8ba869fe4cb426c9afadaeb6ba76130b0b3
Jakub Marciniak <jakub.marciniak@neoncu.be>
2026-05-18 09:22:39 +0200

    feat: integrate Tailwind CSS and refactor components
    - Added Tailwind CSS as a dependency and configured it in vite.config.ts.
    - Replaced the existing CSS styles in main.css with Tailwind CSS imports.
    - Removed HelloWorld and TheWelcome components to streamline the application.
    - Introduced a new NavBar component for navigation.
    - Updated App.vue to utilize the new NavBar and removed unused elements.
    - Modified base.css to include new color variables for a Dracula theme.
    - Cleaned up unused icon components and related files.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <nav class="w-full h-10 flex justify-between px-10 py-4">
+    <div>jamarciniak</div>
+    <div class="flex gap-5">
+      <div>contact</div>
+      <div>about me</div>
+      <div>projects</div>
+    </div>
+  </nav>
+</template>
